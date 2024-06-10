9a0b4989dc47438eccb075a3c93f6f6e116eda98
Alistair Driscoll <pallythecoder@gmail.com>
2024-06-10 19:41:35 +0000

    Add readibility functionality for tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,13 +2,18 @@ document.addEventListener('DOMContentLoaded', function() {
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
 
-
+    // datepicker initialisation
     let datepicker = document.querySelectorAll('.datepicker');
     M.Datepicker.init(datepicker, {
         format: "dd mmmm yyyy",
         i18n: {done: "Select"}
     });
 
+    // selects initialisation
     let selects = document.querySelectorAll('select');
     M.FormSelect.init(selects);
+
+    // collapsible initialisation
+    let collapsibles = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(collapsibles);
 });
